e40c1385dc21cf4ca2eb4383bc1fcc3aa903e55f
invictus89 <1nvictus@naver.com>
2019-05-13 00:05:59 +0900

    Modi

--- tour/src/main/resources/blog.sql
+++ tour/src/main/resources/blog.sql
@@ -2,7 +2,30 @@ DROP TABLE POST_ATTACHMENT;
 DROP TABLE POST;
 DROP TABLE BLOG;
 
+create table board_attachment(
+  attachment_id number primary key,
+  board_id number,
+  original_name varchar2(100),
+  file_size number,
+  update_date number,
+  constraint FK_board_id foreign key(board_id) references board(board_id)
+);
+alter table board_attachment drop constraint FK_board_id;
+alter table board_attachment add constraint FK_board_attchement foreign key(board_id)
+references board(board_id) on delete cascade;
 
+create table board(
+  board_id number primary key,
+  title varchar2(256),
+  writer varchar2(50 byte),
+  password varchar2(256),
+  content varchar2(1024),
+  read_cnt number,
+  reg_date date default sysdate,
+  update_date date default sysdate
+);
+create sequence board_seq;
+drop sequence board_seq;
 CREATE TABLE BLOG(
   blog_id number primary key,
   title varchar2(256),
@@ -46,6 +69,14 @@ CREATE TABLE BOARD_ATTACHMENT(
 );
 
 CREATE SEQUENCE BOARD_ATTACH_SEQ;
-
-
+SET FOREIGN_KEY_CHECKS = 0;
+select * from board_attachment;
 select * from blog;
+select * from board;
+truncate table board;
+drop table board cascade constraint;
+
+insert into board(board_id, title, writer, password, read_cnt, content) values(board_seq.nextval, 'test1', 'hong', '111111', 0, 'test1');
+
+
+commit;
